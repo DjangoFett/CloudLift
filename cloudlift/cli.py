d0ed829e42b502c244e2a03879e4ae154d95ace6
Myshrall, Jake <JMyshrall@cvent.com>
2018-08-27 12:35:39 -0300

    Added all base files and configuration for package deployment support

---
+++ cloudlift/cli.py
@@ -0,0 +1,10 @@
+"""All interfaces done through the cli are handled through this"""
+
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,48 @@
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+from setuptools import setup, find_packages
+from os import path
+from io import open
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name='CloudLift',
+    version='0.1.0',
+    description='Automates AWS Multi-Account and enforces naming ' \
+                'conventions for an organization.',
+    url='https://github.com/DjangoFett/CloudLift',
+    author='Jake Myshrall',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: System Administrators',
+        'Topic :: Other/Nonlisted Topic',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    install_requires=[], 
+    entry_points={  
+        'console_scripts': [
+            'cloudlift=cloudlift.cli:main',
+        ],
+    },
+    # project_urls={  # Optional
+    #     'Bug Reports': 'https://github.com/pypa/sampleproject/issues',
+    #     'Funding': 'https://donate.pypi.org',
+    #     'Say Thanks!': 'http://saythanks.io/to/example',
+    #     'Source': 'https://github.com/pypa/sampleproject/',
+    # },
+)
